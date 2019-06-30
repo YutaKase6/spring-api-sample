28226576dd6e23e5ff7099f6afb412274038fb19
Yuta Kase <yutkase@yahoo-corp.jp>
2019-06-30 14:28:22 +0900

    use lombok slf4j

--- src/main/java/com/example/springapi/aop/LoggingAspects.java
+++ src/main/java/com/example/springapi/aop/LoggingAspects.java
@@ -1,36 +1,33 @@
 package com.example.springapi.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Slf4j
 @Aspect
 @Component
 public class LoggingAspects {
-
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspects.class);
 
     @Before("within(com.example.springapi.application.controller.UserController)")
     public void controllerStartLog(JoinPoint joinPoint) {
         String string = joinPoint.toString();
         String args = Arrays.toString(joinPoint.getArgs());
 
-        logger.info("Start {}, args: {}", string, args);
+        log.info("Start {}, args: {}", string, args);
     }
 
-    @After("within(com.example.springapi.application.controller.UserController)")
+    @AfterReturning("within(com.example.springapi.application.controller.UserController)")
     public void controllerEndLog(JoinPoint joinPoint) {
         String string = joinPoint.toString();
         String args = Arrays.toString(joinPoint.getArgs());
 
-        logger.info("End {}, args: {}", string, args);
+        log.info("End {}, args: {}", string, args);
     }
 
     @AfterThrowing(value = "within(com.example.springapi.application.controller.UserController)", throwing = "e")
@@ -38,6 +35,6 @@ public class LoggingAspects {
         String string = joinPoint.toString();
         String args = Arrays.toString(joinPoint.getArgs());
 
-        logger.error("Error!, Exception: {}, {}, args = {}", e.getMessage(), string, args);
+        log.error("Error!, Exception: {}, {}, args = {}", e.getMessage(), string, args);
     }
 }
